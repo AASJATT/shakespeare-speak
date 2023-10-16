f9a836cd7d16b39b0de25ad865b6a51b744825df
AASJATT <141618407+AASJATT@users.noreply.github.com>
2023-10-16 12:10:34 +0000

    error updated

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var txtInput= document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output")
 var serverURL= "https://api.funtranslations.com/translate/shakespeare.json"
 
+function errorHandler(error){
+    console.log("error ocurred " , error )
+    alert("something wrong with server! Try again after some time")
+}
+
 function getTranslationURL(text) {
     return serverURL+"?"+"text="+text
 }
@@ -13,7 +18,8 @@ function clickHandler(){
     fetch(getTranslationURL(txtInput.value))
     .then(response => response.json())
     .then(json=> outputDiv.innerText=json.contents.translated)
+    .catch(errorHandler)}
 
     
-}
+
 btnTranslate.addEventListener("click", clickHandler)
